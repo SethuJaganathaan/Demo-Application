50f66c7165b08915bdb4f0a96cb74c4ec5be597d
Sethu J <sethuj99@gmail.com>
2023-09-26 11:12:47 +0530

    Controller UpdateUser Testing

---
+++ Application.UnitTest/ControllerTest/AdminControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Application.Repository.DTO.Admin;
+using Application.Repository.Interfaces;
+using Application.Service.Services;
+using Moq;
+
+namespace Application.UnitTest.ControllerTest
+{
+    public class AdminControllerTest
+    {
+        [Fact]
+        public async Task UpdateUser_ReturnSuccess()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var userUpdateDto = new UserUpdateDTO
+            {
+                Username = "Dummy",
+                Email = "dummy@gmail.com",
+                Password = "dummy@*#5",
+                ProfilePicture = null,
+                RoleId = Guid.NewGuid(),
+                DepartmentId = Guid.NewGuid()
+            };
+
+            var mockRepository = new Mock<IAdminRepository>();
+            mockRepository.Setup(repo => repo.UpdateUser(userId, userUpdateDto))
+                .ReturnsAsync("Action Successful");
+
+            var adminService = new AdminService(mockRepository.Object);
+
+            // Act
+            var result = await adminService.UpdateUser(userId, userUpdateDto);
+
+            // Assert
+            Assert.Equal("Action Successful", result);
+        }
+    }
+}
