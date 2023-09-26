115ee4641950c1bf60d9acfebfb5f6f9c6a93686
Sethu J <sethuj99@gmail.com>
2023-09-26 11:54:20 +0530

    Global Exception Uncommented

--- Application.API/Program.cs
+++ Application.API/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    //app.ConfigureExceptionHandler();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
