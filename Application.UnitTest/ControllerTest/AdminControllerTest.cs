68c3041af2e4fe256cc8e7840f29274e614e3a0b
Sethu J <sethuj99@gmail.com>
2023-09-26 15:54:14 +0530

    Unit Testing True, False, Null

--- Application.UnitTest/ControllerTest/AdminControllerTest.cs
+++ Application.UnitTest/ControllerTest/AdminControllerTest.cs
@@ -34,5 +34,47 @@ namespace Application.UnitTest.ControllerTest
             // Assert
             Assert.Equal("Action Successful", result);
         }
+
+        [Fact]
+        public async Task UpdateUser_UserNotFound()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            var userUpdateDto = new UserUpdateDTO();
+            var output = "Not Found";
+
+            var mockRepository = new Mock<IAdminRepository>();
+            mockRepository.Setup(repo => repo.UpdateUser(userId, userUpdateDto))
+                .ReturnsAsync(output);
+
+            var adminService = new AdminService(mockRepository.Object);
+
+            // Act
+            var result = await adminService.UpdateUser(userId, userUpdateDto);
+
+            // Assert
+            Assert.Equal(output, result);
+        }
+
+        [Fact]
+        public async Task UpdateUser_ReturnNull()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+            UserUpdateDTO userUpdateDTO = null;
+            string output = null;
+
+            var mockRepository = new Mock<IAdminRepository>();
+            mockRepository.Setup(repo => repo.UpdateUser(userId, userUpdateDTO))
+                .ReturnsAsync(output);
+
+            var adminService = new AdminService(mockRepository.Object);
+
+            // Act
+            var result = await adminService.UpdateUser(userId, userUpdateDTO);
+
+            // Assert
+            Assert.Equal(output, result);
+        }
     }
 }
