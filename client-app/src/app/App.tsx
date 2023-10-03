50dab6ed15919a8105ca4a7405b7180fa192cc77
Sethu J <sethuj99@gmail.com>
2023-10-03 11:31:51 +0530

    client app

---
+++ client-app/src/app/App.tsx
@@ -0,0 +1,14 @@
+import Login from "../features/registration/Login";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
