f57fc309d1e9993853811226332182b755d97825
Sethu J <sethuj99@gmail.com>
2023-10-04 13:36:07 +0530

    client-app agent.ts

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,20 +1,20 @@
 import axios, { AxiosResponse } from "axios";
 
 axios.defaults.baseURL = 'https://localhost:7175/';
-const responseBody = <T> (response: AxiosResponse<T>) => response.data;
+
+const responseBody = (response: AxiosResponse) => response.data;
 
 const requests = {
-    //get: <T> (url : string) => axios.get<T>(url).then(responseBody),
-    post:<T> (url : string, body: {}) => axios.post<T>(url,body).then(responseBody),
+    post: <T>(url: string, body: any) => axios.post<T>(url, body).then(responseBody),
 }
 
 const Application = {
-    //list: () => requests.get<>('/'),
-    login: (email: string, password: string) => requests.post<{ status: string, token: string }>('Registration/login', { email, password }),
+    login: (email: string, password: string) =>
+        requests.post<{ status: string; token: string }>('Registration/login', { email, password }),
 }
 
 const agent = {
     Application
-} 
+}
 
 export default agent;
